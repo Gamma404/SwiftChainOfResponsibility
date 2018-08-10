02d8138633193cc1ea707f90a87e087a0288c4a0
am903103 <am903103@au.edu.tw>
2018-08-10 19:10:53 +0800

    Create Mac Command line tool project

---
+++ ChainOfResponsibility/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ChainOfResponsibility
+//
+//  Created by Apple on 2018/8/10.
+//  Copyright © 2018年 Gamma. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
