01a850e50cd216851d79c86d1ec2d199d9dcd04b
am903103 <am903103@au.edu.tw>
2018-08-10 19:28:02 +0800

    情境說明-主程式

--- ChainOfResponsibility/main.swift
+++ ChainOfResponsibility/main.swift
@@ -6,7 +6,31 @@
 //  Copyright © 2018年 Gamma. All rights reserved.
 //
 
-import Foundation
+//三個角色
+let employee = Employee()
+let boss = Boss()
+let ceo = CEO()
 
-print("Hello, World!")
+//角色的關係:鏈結的方向
+employee.nextManagementLevel = boss
+boss.nextManagementLevel = ceo
+
+//開支物件
+//Case 5元
+let expenditure = Expenditure(amount: 5)
+//員工開始往上報 誰可以支付
+employee.shouldApproveExpenditure(expenditure: expenditure)
+
+//Case 500元
+expenditure.amount = 500
+employee.shouldApproveExpenditure(expenditure: expenditure)
+
+//Case 5000元
+expenditure.amount = 5000
+employee.shouldApproveExpenditure(expenditure: expenditure)
+
+//Case 50000元
+expenditure.amount = 50000
+employee.shouldApproveExpenditure(expenditure: expenditure)
+
 
